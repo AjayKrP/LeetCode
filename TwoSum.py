b1de53221caa42d2ed7857cc32df722a5e5eb4ba
ajay <23704820+AjayKrP@users.noreply.github.com>
2020-04-30 17:20:08 +0530

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -6,3 +6,13 @@ class Solution:
             if (tmp in extra):
                 return [extra[tmp], i]
             extra[nums[i]] = i
+
+            
+# worst case complexity
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         extra = {}
+#         for i in range(len(nums)): # O(n)
+#             for j in range(len(nums)): #O(n)*O(n) = O(n^2)
+#                 if (nums[j] == target - nums[i]):
+#                     return [i, j]
