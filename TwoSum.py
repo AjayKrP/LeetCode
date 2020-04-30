1e482c7e02d6665b98aca5620745e2fd51d32a48
ajay <23704820+AjayKrP@users.noreply.github.com>
2020-04-30 17:24:17 +0530

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -13,6 +13,6 @@ class Solution:
 #     def twoSum(self, nums: List[int], target: int) -> List[int]:
 #         extra = {}
 #         for i in range(len(nums)): # O(n)
-#             for j in range(len(nums)): #O(n)*O(n) = O(n^2)
+#             for j in range(i + 1, len(nums)): #O(n)*O(n) = O(n^2)
 #                 if (nums[j] == target - nums[i]):
 #                     return [i, j]
